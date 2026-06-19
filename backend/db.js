a318c967f64e2c2b4897fcd442329e4d1ef4db39
prathibhapenta <prathibhapenta98@gmail.com>
2026-06-19 21:36:05 +0530

    Use Render PostgreSQL

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,10 @@
-const {Pool} = require('pg')
+const { Pool } = require('pg')
 
-const  pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "demotodoDb",
-    password: "Prathibha@73",
-    port: 5432
-
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
+
 module.exports = pool
